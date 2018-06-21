a2c9b0a9e3d1eec3415ca79879332bb9a323c46c
C Mooney <37115255+c-mooney@users.noreply.github.com>
2018-06-21 17:35:14 -0500

    Delete opl.pl

--- opl.pl
+++
@@ -1,8 +0,0 @@
-# perl -pf opl.pl FileName.SFM
-# Converts SFM file to one record per line
-# SFM fields are terminated by '#'
-# pre-existing '#' in the file are changed to '_hash_' 
-chomp;
-print "\n" if /\\lx /;
-s/#/\_\_hash\_\_/g;
-$_ .= "#"
